92599f2ac551330a158ee373a196018ab328effe
flzuazo <flzuazo@gmail.com>
2025-09-27 19:48:51 -0500

    Delete G1-TF directory
    Error de carga

--- G1-TF/src/main/java/com/upc/g1tf/interfaces/IPacienteService.java
+++
@@ -1,7 +0,0 @@
-package com.upc.g1tf.interfaces;
-
-import com.upc.g1tf.dtos.PacienteDTO;
-
-public interface IPacienteService {
-    PacienteDTO registrarPaciente(PacienteDTO pacienteDTO); //HU02
-}
--- G1-TF/src/main/java/com/upc/g1tf/interfaces/IProfesionalSaludService.java
+++
@@ -1,7 +0,0 @@
-package com.upc.g1tf.interfaces;
-
-import com.upc.g1tf.dtos.ProfesionalSaludDTO;
-
-public interface IProfesionalSaludService {
-    ProfesionalSaludDTO registrarProfesional(ProfesionalSaludDTO profesionalSaludDTO); // HU006
-}
