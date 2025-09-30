f6089b001ac9558a607acb5b56be5d498cc5fcbc
flzuazo <flzuazo@gmail.com>
2025-09-29 22:50:42 -0500

    actualización en HU12

--- G1-TF/src/main/java/com/upc/g1tf/repositories/ConsultaRepository.java
+++ G1-TF/src/main/java/com/upc/g1tf/repositories/ConsultaRepository.java
@@ -4,8 +4,6 @@ import com.upc.g1tf.entities.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Integer> {
