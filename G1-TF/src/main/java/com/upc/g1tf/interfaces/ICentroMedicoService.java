ae04b59c74f7c26c50fa65c60f2043e909b2fc4d
Eduardo Flores <flzuazo@gmail.com>
2025-11-26 09:49:26 -0500

    ERROR AL CLONAR PROYECTO

--- G1-TF/src/main/java/com/upc/g1tf/interfaces/ICentroMedicoService.java
+++
@@ -1,7 +0,0 @@
-package com.upc.g1tf.interfaces;
-
-import com.upc.g1tf.dtos.CentroMedicoDTO;
-
-public interface ICentroMedicoService {
-    CentroMedicoDTO registrarCentroMedico(CentroMedicoDTO centromedicoDTO);
-}
