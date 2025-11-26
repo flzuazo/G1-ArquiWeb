2b8a94ae08b90275489ece9972050115a72cf436
FabianC <fabian.colque@gmail.com>
2025-11-25 20:58:49 -0500

    se eliminaron reportes y corrigio registro de centro med y registro de doctores

--- G1-TF/src/main/java/com/upc/g1tf/controllers/ProfesionalSaludController.java
+++ G1-TF/src/main/java/com/upc/g1tf/controllers/ProfesionalSaludController.java
@@ -13,8 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-//@CrossOrigin(origins = "*", allowedHeaders = "*")
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true", exposedHeaders = "Authorization") //para cloud
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true", exposedHeaders = "Authorization")
 @RequestMapping("/api")
 public class ProfesionalSaludController {
     @Autowired
